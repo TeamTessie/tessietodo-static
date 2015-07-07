0863f9883f211b919ed06523661a91799b2b0294
Pilar Huidobro <huidobropilar@gmail.com>
2015-07-07 15:18:26 +0200

    corrects for empty fields

--- Try/code.js
+++ Try/code.js
@@ -1,6 +1,9 @@
 function addTodo() {
     var todoTitle;
     todoTitle = $("#todo-name").val();
+    if (todoTitle === "") {
+        return;
+    }
     $("#tasks").append("<li> <input type=\"checkbox\">" + todoTitle + "</input></li>");
     $("#todo-name").val("");
 }
