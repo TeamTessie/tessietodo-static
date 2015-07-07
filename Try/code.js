6fb03485772dd5da7fc28cfc5a8dba9ea86eadc6
Pilar Huidobro <huidobropilar@gmail.com>
2015-07-07 15:14:46 +0200

    Adds checkboxes and clears forms after input.

--- Try/code.js
+++ Try/code.js
@@ -1,5 +1,6 @@
 function addTodo() {
     var todoTitle;
     todoTitle = $("#todo-name").val();
-    $("#tasks").append("<li>" + todoTitle + "</li>");
-} 
+    $("#tasks").append("<li> <input type=\"checkbox\">" + todoTitle + "</input></li>");
+    $("#todo-name").val("");
+}
