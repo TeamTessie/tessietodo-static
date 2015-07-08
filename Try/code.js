ade39662819b61fcd21bee9e33e8655e780637ab
Pilar Huidobro <huidobropilar@gmail.com>
2015-07-08 11:01:20 +0200

    Adds localstorage but breaks the strikethrough...

--- Try/code.js
+++ Try/code.js
@@ -1,19 +1,28 @@
+var todoArray = [];
+var todoKey = "todos";
+
 function addTodo() {
     var todoTitle;
     todoTitle = $("#todo-name").val();
     if (todoTitle === "") {
         return;
     }
+    todoArray.push(todoTitle);
+    update_storage(todoArray);
     $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + todoTitle + "</input></li>");
     $("#todo-name").val("");
 }
 
 function todoChecked(el) {
- if($(el).is(":checked")===true) {
-   $(el).parent().addClass("checked");
- }
- else {
-   $(el).parent().removeClass("checked");
- }
+    if ($(el).is(":checked")===true){
+        $(el).addClass(".checked");
+    }
 
+    else {
+        $(el).removeClass(".checked");
+    }
+}
+
+function update_storage(todos) {
+    localStorage.setItem(todoKey, JSON.stringify(todos));
 }
