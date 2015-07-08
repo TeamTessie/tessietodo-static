1a3ef258118e79b64740baf32ff4b6a05fadbe89
Pilar Huidobro <huidobropilar@gmail.com>
2015-07-08 12:21:15 +0200

    Displaying stored items works

--- Try/code.js
+++ Try/code.js
@@ -1,40 +1,42 @@
 var todoArray = [];
 var todoKey = "todos";
 
-// check whether we have todos in the localStorage
-if (localStorage.getItem(todoKey)) {
-    //1. Add the todos into the todoArray
-    var rawJSON = localStorage.getItem(todoKey)
-    todoArray = JSON.parse(rawJSON);
-    //2. Populate list with Todos
-    // Go through each todo in the array
-    // and add it to our html list, so it looks like
-    // It's loaded
-    todoArray.forEach(function(task) {
-        $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + task + "</input></li>");
-    });
-}
-
-function addTodo() {
-    var todoTitle;
-    todoTitle = $("#todo-name").val();
-    if (todoTitle === "") {
-        return;
+$(document).ready(function() {
+    // check whether we have todos in the localStorage
+    if (localStorage.getItem(todoKey)) {
+        //1. Add the todos into the todoArray
+        var rawJSON = localStorage.getItem(todoKey)
+        todoArray = JSON.parse(rawJSON);
+        //2. Populate list with Todos
+        // Go through each todo in the array
+        // and add it to our html list, so it looks like
+        // It's loaded
+        todoArray.forEach(function(task) {
+            $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + task + "</input></li>");
+        });
     }
-    todoArray.push(todoTitle);
-    update_storage(todoArray);
-    $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + todoTitle + "</input></li>");
-    $("#todo-name").val("");
-}
+});
 
-function todoChecked(el) {
-    if ($(el).is(":checked") === true) {
-        $(el).addClass(".checked");
-    } else {
-        $(el).removeClass(".checked");
+    function addTodo() {
+        var todoTitle;
+        todoTitle = $("#todo-name").val();
+        if (todoTitle === "") {
+            return;
+        }
+        todoArray.push(todoTitle);
+        update_storage(todoArray);
+        $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + todoTitle + "</input></li>");
+        $("#todo-name").val("");
     }
-}
 
-function update_storage(todos) {
-    localStorage.setItem(todoKey, JSON.stringify(todos));
-}
+    function todoChecked(el) {
+        if ($(el).is(":checked") === true) {
+            $(el).addClass(".checked");
+        } else {
+            $(el).removeClass(".checked");
+        }
+    }
+
+    function update_storage(todos) {
+        localStorage.setItem(todoKey, JSON.stringify(todos));
+    }
