52eb5661a52da39c0bcfc11606137f49792308a4
Pilar Huidobro <huidobropilar@gmail.com>
2015-07-08 12:16:09 +0200

    Adds updates for Shelly

--- Try/code.js
+++ Try/code.js
@@ -1,6 +1,20 @@
 var todoArray = [];
 var todoKey = "todos";
 
+// check whether we have todos in the localStorage
+if (localStorage.getItem(todoKey)) {
+    //1. Add the todos into the todoArray
+    var rawJSON = localStorage.getItem(todoKey)
+    todoArray = JSON.parse(rawJSON);
+    //2. Populate list with Todos
+    // Go through each todo in the array
+    // and add it to our html list, so it looks like
+    // It's loaded
+    todoArray.forEach(function(task) {
+        $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + task + "</input></li>");
+    });
+}
+
 function addTodo() {
     var todoTitle;
     todoTitle = $("#todo-name").val();
@@ -14,11 +28,9 @@ function addTodo() {
 }
 
 function todoChecked(el) {
-    if ($(el).is(":checked")===true){
+    if ($(el).is(":checked") === true) {
         $(el).addClass(".checked");
-    }
-
-    else {
+    } else {
         $(el).removeClass(".checked");
     }
 }
