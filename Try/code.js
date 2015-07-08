4f6398f0840e51c24d213da9ec80aa2692f70c93
Pilar Huidobro <huidobropilar@gmail.com>
2015-07-08 14:51:19 +0200

    Adds Tessie Banner and the app functionality is now complete

--- Try/code.js
+++ Try/code.js
@@ -17,26 +17,50 @@ $(document).ready(function() {
     }
 });
 
-    function addTodo() {
-        var todoTitle;
-        todoTitle = $("#todo-name").val();
-        if (todoTitle === "") {
-            return;
-        }
-        todoArray.push(todoTitle);
-        update_storage(todoArray);
-        $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + todoTitle + "</input></li>");
-        $("#todo-name").val("");
-    }
+function clearCheckedTodos() {
+    // 1. Use Jquery to loop through all the checked items
+    // https://api.jquery.com/each/
+    $(".checked").each(function() {
+        // when using the jquery each function, 'this' is holding
+        // the checked item we're currently on. Let's store it on a variable
+        var item = this;
 
-    function todoChecked(el) {
-        if ($(el).is(":checked") === true) {
-            $(el).parent().addClass("checked");
-        } else {
-            $(el).parent().removeClass("checked");
+        var todoToRemove;
+        // get the text content (the title) of the
+        // list item and trim the white spaces from
+        // before and after
+        todoToRemove = item.textContent.trim();
+        var i = todoArray.indexOf(todoToRemove);
+        if (i != -1) {
+            todoArray.splice(i, 1);
         }
-    }
+    });
+    update_storage(todoArray);
+    // remove all elements with the checked class
+    $(".checked").remove();
+}
 
-    function update_storage(todos) {
-        localStorage.setItem(todoKey, JSON.stringify(todos));
+
+function addTodo() {
+    var todoTitle;
+    todoTitle = $("#todo-name").val();
+    if (todoTitle === "") {
+        return;
     }
+    todoArray.push(todoTitle);
+    update_storage(todoArray);
+    $("#tasks").append("<li> <input type=\"checkbox\" onclick=\"todoChecked(this)\">" + todoTitle + "</input></li>");
+    $("#todo-name").val("");
+}
+
+function todoChecked(el) {
+    if ($(el).is(":checked") === true) {
+        $(el).parent().addClass("checked");
+    } else {
+        $(el).parent().removeClass("checked");
+    }
+}
+
+function update_storage(todos) {
+    localStorage.setItem(todoKey, JSON.stringify(todos));
+}
