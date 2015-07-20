bd8b1f6d61bfa6d15d4cc3710dcd556ea36ac730
Shellycoen <michelle_coen@yahoo.com>
2015-07-20 09:16:04 +0200

    adding changes from editey

--- CouchDBTest/code.js
+++ CouchDBTest/code.js
@@ -1,5 +1,21 @@
+// This is not a function, so this is run as soon as the program is loaded.
 $.ajax({
-  url: "http://localhost:5984/tessieapp",
+// This displays all the docs inside the specified database
+  url: "http://localhost:5984/tessieapp/my_id",
+  method: "PUT",
+  contentType: "application/json",
+  data: JSON.stringify({"task": "Upload new task"}),
+  success: function(data) {
+      console.log(data);
+  },
+  failure: function() {
+      console.log("Awwwww");
+  }
+});
+
+// THIS ONE IS FOR GETTING. (it's not forgetting.)
+$.ajax({
+  url: "http://localhost:5984/tessieapp/my_id",
   success: function(data) {
       console.log(data);
   },
